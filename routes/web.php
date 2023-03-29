4c686c7a675ae931b56b18fc3d24dc5548bfebaf
alikerock <alikerock@gmail.com>
2023-03-29 18:05:18 +0900

    route url updated

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,21 @@ Route::get('/', function () {
     return view('welcome');
     // return '<h1>hello laravel</h1>';
 });
+
 Route::get('/about', function () {    
     return '<h2>about page</h2>';
+});
+
+// Route::get('/store', function () {
+//     $category = request('category');
+//     return strip_tags($category).'용 스토어 페이지를 보고 있습니다.';
+// });
+
+Route::get('/store', function () {
+    $category = request('category');
+
+    if(isset($category)){
+        return '스토어 페이지에서 '.strip_tags($category).' 카테고리를  보고 있습니다.';
+    }
+    return '스토어 페이지의 모든 제품을 보고 있습니다.';
 });
